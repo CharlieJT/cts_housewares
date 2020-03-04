834c942ae16cfe029c0d05de9bc0ab40fc58eb18
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-04 14:47:09 +0000

    Implement bootstrap forms to give forms a better & sharper look & style

--- cts_housewares/settings.py
+++ cts_housewares/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'django_forms_bootstrap',
+
+    # my apps
     'account',
     'home',
 ]
