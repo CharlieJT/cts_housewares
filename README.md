32199705c45b04e3385cf1010d40523f979b724e
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-04 11:46:16 +0000

    Repo synced with travis for continuous integration testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# CT's Housewares
+
+[![Build Status](https://travis-ci.org/CharlieJT/cts_housewares.svg?branch=master)](https://travis-ci.org/CharlieJT/cts_housewares)
--- cts_housewares/urls.py
+++ cts_housewares/urls.py
@@ -19,5 +19,5 @@ from home.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
+    path('', home_page, name="home_page"),
 ]
