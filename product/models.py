ceb90a821708a38ad7da16e4b4e1d7ff07035758
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-06 15:14:50 +0000

    Add card layout to html in products & add about product field to django admin

--- product/models.py
+++ product/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     item_number = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
+    about_product = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
     image = models.ImageField(upload_to='images')
     stock = models.IntegerField(blank=True, null=True)
