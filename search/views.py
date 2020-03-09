ab2ef3a3f81df77e661675d131ec7091323991c8
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-09 13:21:07 +0000

    Quick adjustment to search & only show checkout button if the user is authenticated

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 # Create your views here.
 def search_products(request):
+    """ This is responsible for searching for products querying on item number, description & about product fields in product database """
     query = request.GET.get('q')
     products = Product.objects.filter(Q(item_number__icontains=query) |
                                     Q(description__icontains=query) |
