5de3fd792dd14c8381f5979247c20b703b72077b
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-09 10:02:59 +0000

    Add functionality to add products to the basket

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,5 @@ from .views import view_cart, add_to_cart, adjust_cart
 urlpatterns = [
     path('', view_cart, name="view_cart"),
     path('add/<int:id>', add_to_cart, name="add_to_cart"),
-    path('adjust/<int:id>', add_to_cart, name="add_to_cart"),
+    path('adjust/<int:id>', add_to_cart, name="adjust_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -10,7 +10,10 @@ def add_to_cart(request, id):
     quantity=int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
 
@@ -21,6 +24,7 @@ def adjust_cart(request, id):
     quantity=int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
+    print(quantity)
     if quantity > 0:
         cart[id] = quantity
     else:
