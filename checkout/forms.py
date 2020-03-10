a74af437343f3fe3d0525b12dcc493390dc217f0
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-10 08:49:52 +0000

    Create form to be able to be abel to make an order

---
+++ checkout/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Order
+
+class MakePaymentForm(forms.Form):
+
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
+
+    credit_card_number = forms.CharField(label="Credit Card Number", required=False)
+    cvv = forms.CharField(label="Security Code(CVV)", required=False)
+    expiry_month = forms.CharField(label="Month", choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.CharField(label="Year", choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Orderfields = (
+            'first_name',
+            'last_name',
+            'address_line_1',
+            'address_line_2',
+            'address_line_3',
+            'town_or_city',
+            'postcode',
+            'country',
+            'phone_number',
+            'date'
+        )
