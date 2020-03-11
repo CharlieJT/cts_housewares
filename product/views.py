93c7a48754167dd7d0b22a46f5ad41ffd0e147b7
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-11 12:55:00 +0000

    Add pagination to product page & to work with querying on search page

--- product/views.py
+++ product/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def all_products(request):
+
     products = Product.objects.all()
+    
+    paginator = Paginator(products, 9)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
     return render(request, "products.html", {"products": products})
--- search/views.py
+++ search/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from product.models import Product
 from django.db.models import Q
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def search_products(request):
     """ This is responsible for searching for products querying on item number, description & about product fields in product database """
-    query = request.GET.get('q')
-    products = Product.objects.filter(Q(item_number__icontains=query) |
-                                    Q(description__icontains=query) |
-                                    Q(about_product__icontains=query))
+    if request.GET:
+        query = request.GET.get('q', '')
+        products = Product.objects.filter(Q(item_number__icontains=query) |
+                                        Q(description__icontains=query) |
+                                        Q(about_product__icontains=query))
 
-    return render(request, "products.html", {'products': products})
+    page_request_var = "q={}&".format(query)
+    paginator = Paginator(products, 9)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+
+    return render(request, "products.html", {'products': products, "page_request_var": page_request_var})
