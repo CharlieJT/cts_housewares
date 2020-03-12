8a48a249722eb284f5e7551a18975b3bec21c8ac
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-12 16:48:53 +0000

    Add box shadow to navbar depending on whether the window is scrolled down the page or not

--- static/js/script.js
+++ static/js/script.js
@@ -8,12 +8,12 @@ $(document).ready(() => {
     // Will check to see if the window has been scrolled 50px from the top.
     $(window).scroll(() => {
         if ($(document).scrollTop() > 50) {
-            $(navbarContainer).css("height", '66px');
+            $(navbarContainer).css({ "height": "66px", "box-shadow": "0 2px 5px #5f5f5f" });
             $(navbarLogo).css("display", "none");
             $(navbarLogoScrolled).css("display", "block");
         }
         else {
-            $(navbarContainer).css("height", '92px');
+            $(navbarContainer).css({ "height": "92px", "box-shadow": "none" });
             $(navbarLogo).css("display", "block");
             $(navbarLogoScrolled).css("display", "none");
         }
