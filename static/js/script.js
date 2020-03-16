4414f480371d644e8954dbb41ecb4eae3039652f
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-16 09:21:55 +0000

    Add animation to burger icon using transform changing to a cross when sidedrawer is active

--- static/js/script.js
+++ static/js/script.js
@@ -8,6 +8,9 @@ const mainProductImage = document.getElementsByClassName("main-product-image");
 const navbarToggler = document.getElementsByClassName("navbar-toggler");
 const mobileNavbarContainer = document.getElementsByClassName("mobile-navbar-container");
 const backdrop = document.getElementsByClassName("backdrop");
+const burgerIcon1 = document.getElementsByClassName("burger-icon-1");
+const burgerIcon2 = document.getElementsByClassName("burger-icon-2");
+const burgerIcon3 = document.getElementsByClassName("burger-icon-3");
 
 
 $(document).ready(() => {
@@ -36,9 +39,15 @@ $(document).ready(() => {
         if ($(navbarToggler).hasClass("collapsed")) {
             $(mobileNavbarContainer).css("right", "-70%");
             $(backdrop).css("display", "none");
+            $(burgerIcon1).css("transform", "translateY(-3px) rotate(0deg)");
+            $(burgerIcon2).css({"transform": "translateX(0px)", "opacity": "1"});
+            $(burgerIcon3).css("transform", "translateY(3px) rotate(0deg)");
         } else {
             $(mobileNavbarContainer).css("right", "0");
             $(backdrop).css("display", "block");
+            $(burgerIcon1).css("transform", "translateY(4px) rotate(-135deg)");
+            $(burgerIcon2).css({"transform": "translateX(-50px)", "opacity": "0"});
+            $(burgerIcon3).css("transform", "translateY(-4px) rotate(135deg)");
         }
     });
 });
