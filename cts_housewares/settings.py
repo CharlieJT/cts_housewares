95e3377d9490366b348b775a5c9d9b748d5573a2
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-17 15:54:47 +0000

    Modify databases in an if else statement to get travis passing

--- cts_housewares/settings.py
+++ cts_housewares/settings.py
@@ -95,13 +95,16 @@ WSGI_APPLICATION = 'cts_housewares.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
