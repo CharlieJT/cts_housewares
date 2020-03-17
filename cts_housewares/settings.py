b92c49ad8b370c5fd2f30c248cf2b6e3de78ddec
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-17 16:57:16 +0000

    Fixing images showing in heroku app

--- cts_housewares/settings.py
+++ cts_housewares/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cts-housewares.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "cts-housewares.herokuapp.com"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'cts_housewares.wsgi.application'
 
 
 if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 else:
     DATABASES = {
         'default': {
