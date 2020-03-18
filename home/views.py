f51bf0ecadb5f4fbe2c3d2aeae13b84e1e6f4b5f
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-03-18 15:56:01 +0000

    Allow searching by brand in the dropdown in the navbar

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Banner
+from product.models import Product
 
 # Create your views here.
 def home_page(request):
     """ This will return the home page """
+
     banners = Banner.objects.all()
 
     return render(request, "index.html", { "banners": banners })
